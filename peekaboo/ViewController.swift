217c538a2b5fc79319fe585bc99b3262a9daee6b
Suzie <32041993+suziebytes@users.noreply.github.com>
2022-11-28 20:08:01 -0800

    Created launch screen + main home screen; revision on logo resizing and constraints needed

--- peekaboo/ViewController.swift
+++ peekaboo/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    // When start is clicked
+    @IBAction func pressStart(_ sender: UIButton) {
+        print("clicked")
+    }
 }
+
+
 
