5879ae106e1b17c669d033c2f641396810180c4f
Suzie <reorsusie@gmail.com>
2022-12-02 17:14:31 -0800

    add viewWillAppear for animalCard

--- peekaboo/animalViewController.swift
+++ peekaboo/animalViewController.swift
@@ -11,19 +11,31 @@ import UIKit
 class animalViewController: UIViewController {
        
     @IBOutlet weak var backToPeopleView: UIButton!
-//
-//
     @IBAction func onAnimalTap(_ sender: UIButton) {
-//        let randomNumber =
         let peopleImage = UIImage(named: "(randomNumber)")
 
     }
+    @IBOutlet weak var animalImage: UIImageView!
     
     //called only once
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var animalImages = ["Ant", "Bee", "Bird", "Cat", "Chicken", "Cow", "Dog", "Duck", "Giraffe", "Horse", "JellyFish", "Lion", "Lizard", "Monkey", "Panda", "Pig", "Shark", "Skunk", "Tiger", "Zebra"]
+    
+    let randNum = Int.random(in: 0..<21)
+    
+    //called everytime the specfic view is appeared
+    override func viewWillAppear(_ animated: Bool) {
+
+        super.viewWillAppear(animated)
+        animalImage.image = UIImage(named: animalImages[randNum])
+        
+        print("animal card view works")
+    }
+
     
   
 
